07a85aa79e846eb2095c15ffbfc24f631558deb7
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-08-21 00:08:32 +0300

    Do not always redirect to the active combined process
    In case the user exits the process views, do not redirect the
    user back to the process in case they go to the authorizations.

--- lib/decidim/combined_budgeting/middleware/authorization_redirect.rb
+++ lib/decidim/combined_budgeting/middleware/authorization_redirect.rb
@@ -10,16 +10,53 @@ module Decidim
 
         def call(env)
           req = Rack::Request.new(env)
-          process_id = req.session["active_combined_budgeting_process_id"]
-          if process_id && req.path == "/authorizations" && req.get?
-            process = CombinedBudgeting::Process.find_by(id: process_id)
-            return redirect("/budgeting/#{process.slug}") if process
+          if req.get? && !in_verification?(req)
+            if req.path == "/authorizations"
+              response = budgeting_redirect_for(req)
+              return response if response
+            else
+              # The user is outside of a verification process and is not in the
+              # authorizations root, so we don't need to store this information
+              # anymore.
+              req.session.delete("active_combined_budgeting_process_id")
+            end
           end
 
           @app.call(env)
         end
 
         private
+
+        def budgeting_redirect_for(request)
+          process_id = request.session.delete(
+            "active_combined_budgeting_process_id"
+          )
+          return unless process_id
+
+          process = CombinedBudgeting::Process.find_by(id: process_id)
+          redirect("/budgeting/#{process.slug}") if process
+        end
+
+        def in_verification?(request)
+          available_verifications_for(request).any? do |verification|
+            check_path = verification.root_path.sub(%r{/$}, "")
+            # One of the following:
+            # /authorization_name
+            # /authorization_name/
+            # /authorization_name/something
+            request.path == check_path || request.path =~ %r{^#{check_path}/.*}
+          end
+        end
+
+        def available_verifications_for(request)
+          organization = request.env["decidim.current_organization"]
+          return [] unless organization
+
+          Verifications::Adapter.from_collection(
+            organization.available_authorizations &
+            Decidim.authorization_workflows.map(&:name)
+          )
+        end
 
         def redirect(location)
           [
